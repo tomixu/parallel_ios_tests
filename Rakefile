28b03dc752c5e8624ffe42dd1e0adf553016483b
Johannes Plunien <plu@pqpq.de>
2016-11-25 07:38:28 +0400

    Use pxctest

--- Rakefile
+++ Rakefile
@@ -6,88 +6,27 @@ require 'benchmark'
 $stdout.sync = true
 $stderr.sync = true
 
-if ENV['TRAVIS']
-  SimCtl.default_timeout = 300
-end
-
 task :default => 'test'
 desc 'Execute tests in multiple simulators'
 task :test, [:specific_test] do |t, args|
-  exit_code = 0
+  sh [
+    'set -o pipefail;',
+    'xcodebuild',
+    '-IDEBuildLocationStyle=Custom',
+    '-IDECustomBuildLocationType=Absolute',
+    "-IDECustomBuildProductsPath='#{Dir.pwd}/build/Products'",
+    '-scheme SampleApp',
+    '-workspace SampleApp.xcworkspace',
+    '-destination "platform=iOS Simulator,name=iPhone 5,OS=10.1"',
+    'build-for-testing',
+    '| xcpretty',
+  ].join(' ')
 
-  args.with_defaults(:specific_test => :all)
-  if args[:specific_test] != :all
-    test_scope = args.extras.inject("-only #{args[:specific_test]}") do |tests, test|
-      tests + ',' + test
-    end
-  else
-    test_scope = nil
-  end
-
-  time = Benchmark.measure do
-    xctool = File.join(Dir.pwd, 'xctool', 'xctool.sh')
-    xctool = "#{xctool} -derivedDataPath '#{File.join(Dir.pwd, 'build')}' -scheme SampleApp -sdk iphonesimulator -workspace SampleApp.xcworkspace"
-
-    # Destroy and create devices for given name, type and os version
-    devices = [
-      SimCtl.reset_device('SampleApp iPhone', SimCtl.devicetype(name: 'iPhone 5'),    SimCtl::Runtime.latest(:ios)),
-      SimCtl.reset_device('SampleApp iPad',   SimCtl.devicetype(name: 'iPad Retina'), SimCtl::Runtime.latest(:ios)),
-    ]
-    devices.each do |device|
-      # This is necessary because Travis seems to be slow...
-      device.wait!{|d| File.exists?(d.path.device_plist)}
-      device.launch!
-    end
-
-    # Build tests once
-    sh "#{xctool} build-tests"
-
-    threads = []
-
-    # Run tests on each device in a separate thread
-    devices.each do |device|
-      threads << Thread.new do
-        test_log = File.join(Dir.pwd, 'build', "#{device.name}.log")
-        junit_xml = File.join(Dir.pwd, 'build', "#{device.name}.junit.xml")
-        if test_scope
-          system "#{xctool} run-tests #{test_scope} -destination 'id=#{device.udid}' -reporter pretty -reporter plain:'#{test_log}' -reporter junit:'#{junit_xml}'"
-        else
-          system "#{xctool} run-tests -destination 'id=#{device.udid}' -reporter pretty -reporter plain:'#{test_log}' -reporter junit:'#{junit_xml}'"
-        end
-        Thread.current[:result] = $?
-        device.kill!
-        device.wait! {|d| d.state == :shutdown}
-        device.delete!
-      end
-    end
-
-    # Wait for all threads to finish
-    threads.each do |thread|
-      thread.join
-      exit_code |= thread[:result].to_i
-    end
-  end
-
-  puts "Total time: #{time.real}s"
-
-  exit exit_code
-end
-
-desc 'Launch app in multiple simulators'
-task :launch do
-  devices = [
-    SimCtl.reset_device('SampleApp iPhone', SimCtl.devicetype(name: 'iPhone 5'),    SimCtl::Runtime.latest(:ios)),
-    SimCtl.reset_device('SampleApp iPad',   SimCtl.devicetype(name: 'iPad Retina'), SimCtl::Runtime.latest(:ios)),
-  ]
-  devices.each { |device| device.launch! }
-
-  build_path = File.join(Dir.pwd, 'build')
-  xctool = File.join(Dir.pwd, 'xctool', 'xctool.sh')
-  xctool = "#{xctool} -derivedDataPath '#{build_path}' -scheme SampleApp -sdk iphonesimulator -workspace SampleApp.xcworkspace"
-  sh "#{xctool} build"
-
-  devices.each do |device|
-    device.install!(File.join(build_path, 'Build/Products/Debug-iphonesimulator/SampleApp.app'))
-    device.launch_app!('com.plunien.SampleApp')
-  end
+  sh [
+    'pxctest',
+    'run-tests',
+    '--destination "name=iPhone 5,os=iOS 9.3"',
+    '--destination "name=iPad Retina,os=iOS 9.3"',
+    '--testrun build/Products/SampleApp_iphonesimulator10.1-i386.xctestrun',
+  ].join(' ')
 end
--- SampleAppTests/SampleAppTests.m
+++ SampleAppTests/SampleAppTests.m
@@ -24,4 +24,504 @@
     }
 }
 
+- (void)test1
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test2
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test3
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test4
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test5
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test6
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test7
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test8
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test9
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test10
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test11
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test12
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test13
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test14
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test15
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test16
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test17
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test18
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test19
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test20
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test21
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test22
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test23
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test24
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test25
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test26
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test27
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test28
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test29
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test30
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test31
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test32
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test33
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test34
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test35
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test36
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test37
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test38
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test39
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test40
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test41
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test42
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test43
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test44
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test45
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test46
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test47
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test48
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test49
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test50
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test51
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test52
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test53
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test54
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test55
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test56
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test57
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test58
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test59
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test60
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test61
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test62
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test63
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test64
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test65
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test66
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test67
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test68
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test69
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test70
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test71
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test72
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test73
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test74
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test75
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test76
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test77
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test78
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test79
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test80
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test81
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test82
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test83
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test84
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test85
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test86
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test87
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test88
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test89
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test90
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test91
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test92
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test93
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test94
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test95
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test96
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test97
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test98
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test99
+{
+    XCTAssertTrue(YES);
+}
+
+- (void)test100
+{
+    XCTAssertTrue(YES);
+}
+
 @end
